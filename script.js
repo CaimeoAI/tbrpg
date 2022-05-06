9df2ffa4e4a7eaa5fb1a55320fe2834c036133c9
Judielle Villa <judiellezinke@yahoo.de>
2022-05-06 16:54:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const prompt = require("prompt-sync")({sigint:false});
+
+//! VARIABLES
+
+
+
+
+
+
+
+
+
+
+
+
+
+//! FUNCTIONS
+
+
+
+
+
+
+
+
+
+
+
+
+//! EVENT RANDOMIZER
+
+// Creates Random Number Between 1-10
+
+function eventGen() {
+    currenEventNumber = Math.round(Math.random()*9+1)
+
+    if(currentEventNumber === 1) {
+        console.log("");
+    }
+}
+
+//! MAINLOOP
+
+console.log("WELCOME TO THE TEXT-BASED RPG!");
+startGame = prompt("Do you want to start the game? [y]/[n]")
+
+if(startGame === "y") {
+
+    for(i=0 , i<10 , i++){
+        
+    }
+
+}
