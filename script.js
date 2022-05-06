fe8fd44b882cfa2ea3a7a9e8578e2ee9df71e76c
Judielle Villa <judiellezinke@yahoo.de>
2022-05-06 17:31:29 +0200

    started the tutorial and sorted code

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const prompt = require("prompt-sync")({sigint:false});
 
 //! VARIABLES
 
-
-
+const prompt = require("prompt-sync")({sigint:false});
+const lineBreak = '\n';
 
 
 
@@ -29,10 +28,9 @@ const prompt = require("prompt-sync")({sigint:false});
 
 //! EVENT RANDOMIZER
 
-// Creates Random Number Between 1-10
-
+// Creates Random Number Between 1-10 and loads the corresponding event to that number
 function eventGen() {
-    currenEventNumber = Math.round(Math.random()*9+1)
+    const currentEventNumber = Math.round(Math.random()*9+1)
 
     if(currentEventNumber === 1) {
         console.log("");
@@ -42,9 +40,19 @@ function eventGen() {
 //! MAINLOOP
 
 console.log("WELCOME TO THE TEXT-BASED RPG!");
-startGame = prompt("Do you want to start the game? [y]/[n]")
+const startGame = prompt("Do you want to start the game? [y]/[n]")
 
 if(startGame === "y") {
+
+    //TODO Start of the Tutorial
+    
+    console.log("\n You wake up in a dark prison cell with your right foot chained to the wall.");
+    console.log("\n As you tried to break the chains that are binding you to these cold stone walls, you notice a saw right next to you.");
+    console.log("\n You take saw into your hands. But what do you want to with it now?")
+
+    const tutorialEvent01 = ("[a] Saw your left foot off [b] Saw your right foot off [c] Saw the the chain off [d] Throw the saw away")
+
+    //TODO End of the Tutorial
 
     for(i=0 ; i<10 ; i++) {
         eventGen()
