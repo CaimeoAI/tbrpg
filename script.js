02f4bca5d524d8db3f1492da2a43f2824b7ab194
Judielle Villa <judiellezinke@yahoo.de>
2022-05-06 17:35:55 +0200

    created do while loop around tutorialevent01 so that it loops when not progressing

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 const prompt = require("prompt-sync")({sigint:false});
 const lineBreak = '\n';
-
+let eventContinue = false;
 
 
 
@@ -45,12 +45,20 @@ const startGame = prompt("Do you want to start the game? [y]/[n]")
 if(startGame === "y") {
 
     //TODO Start of the Tutorial
-    
+
     console.log("\n You wake up in a dark prison cell with your right foot chained to the wall.");
     console.log("\n As you tried to break the chains that are binding you to these cold stone walls, you notice a saw right next to you.");
     console.log("\n You take saw into your hands. But what do you want to with it now?")
 
-    const tutorialEvent01 = ("[a] Saw your left foot off [b] Saw your right foot off [c] Saw the the chain off [d] Throw the saw away")
+    do {
+        
+        const tutorialEvent01 = ("[a] Saw your left foot off [b] Saw your right foot off [c] Saw the the chain off [d] Throw the saw away")
+
+        if (tutorialEvent01 === "a") {
+
+        }
+
+    } while (eventContinue=false)
 
     //TODO End of the Tutorial
 
