14f1af2df5ee5a6e000f7f6df085c646b99038c9
Judielle Villa <judiellezinke@yahoo.de>
2022-05-09 11:37:18 +0200

    created and implemented gameOverSwitch and finished rest function and restHeal function

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let playerGold = 0;
 let maxHealth = 10;
 let restContinue = false;
 let event01Done = false;
+let gameOverSwitch = false;
 
 //! PLAYER STATS OBJECT DECLARATION and PLAYER STAT FUNCTIONS
 
@@ -35,6 +36,7 @@ function statIncrease(stat,num) {
 
 function gameOver() {
     console.log("\n \n GAME OVER \n \n");
+    gameOver = true;
 }
 
 //! INVALID CHOICE FUNCTION
@@ -43,21 +45,43 @@ function invaidChoice() {
     console.log("\n Invalid Choice. Try using the letters corresponding to the given choices!");
 }
 
+//! REST HEAL FUNCTION
+
+function restHeal() {
+    playerStats.Health = maxHealth;
+    console.log("You take a rest... \n \n");
+    console.log(`You are completey healed back up to ${playerStats.Health}! \n \n`);
+}
+
 //! REST FUNCTION
 
 function rest() {
+
     do {
 
         console.log("You are resting.");
         console.log("[c]Character Stats");
         console.log("[i]Inventory");
-        console.log("[r]Rest");
+        console.log("[r]Rest to heal");
         console.log("[w]Continue Journey");
 
         let restChoice = prompt(" What do you want to do?       PICK A CHOICE:");
 
-        if (restChoice === c) {
-
+        if (restChoice === "c") {
+            console.clear();
+            playerStatsShow();
+        } else if (restChoice === "i") {
+            console.clear();                    // TODO UNDER CONSTRUCTION
+        } else if (restChoice === "r") {
+            console.clear();
+            restHeal() 
+        } else if (restChoice === "w") {
+            console.clear();
+            console.log("\n You get up and continue your Journey... \n");
+            restContinue = true;
+        } else {
+            console.clear();
+            invaidChoice();
         }
 
     } while (restContinue === false) 
@@ -71,7 +95,7 @@ function rest() {
 function eventGen() {
     const currentEventNumber = Math.ceil(Math.random()*10)
 
-    if(currentEventNumber === 1 && event01Done === false) {
+    if(currentEventNumber === 1 && event01Done === false && gameOverSwitch === false) {
 
         console.log("\n You encounter a crossroad. There are three ways to ahead of you.");
 
@@ -82,13 +106,13 @@ function eventGen() {
             let event01 = prompt("[a] Take left road [b] Take road ahead [c] Take right road [d] Turn back      PICK A CHOICE:")
 
             if (event01 === "a") {
-
+                break
             } else if (event01 === "b") {
-
+                event01Done = true;
             } else if (event01 === "c") {
-
+                break
             } else if (event01 === "d") {
-
+                break
             } else {
                 invaidChoice()
             }
@@ -108,13 +132,14 @@ if(startGame === "y") {
 
     //TODO Start of the Tutorial
 
+    console.clear();
     console.log("\n You wake up in a dark prison cell with your right foot chained to the wall.");
     console.log("\n As you tried to break the chains that are binding you to these cold stone walls, you notice a saw right next to you.");
-    console.log("\n You take saw into your hands. But what do you want to with it now? \n \n")
+    console.log("\n You take saw into your hands. But what do you want to with it now? \n \n");
 
     do {
 
-        let tutorialEvent01 = prompt("[a] Saw your left foot off [b] Saw your right foot off [c] Saw the the chain off [d] Throw the saw away       PICK A CHOICE: ")
+        let tutorialEvent01 = prompt("[a] Saw your left foot off [b] Saw your right foot off [c] Saw the the chain off [d] Throw the saw away       PICK A CHOICE: ");
 
         if (tutorialEvent01 === "a") {
             console.clear()
@@ -138,24 +163,24 @@ if(startGame === "y") {
             invaidChoice()
         }
 
-    } while (eventContinue===false)
+    } while (eventContinue === false && gameOverSwitch === false)
 
-    if (eventContinue===true) {
+    if (eventContinue === true) {
 
     console.log("\n You slowly open the door and walk through it and find yourself outside in the middle of a forest.");
     console.log("\n You take a long rest after spending so much energy on freeing yourself from the chains.");
     console.log("\n ...\n \n");
 
-    
-
-    console.log("\n After a while you stand up and head in a random direction into the woods...");
-
     }
     
     //TODO End of the Tutorial
+    
+    if (eventContinue === true && gameOverSwitch === false) {
 
-    for(i=0 ; i<10 ; i++) {
-        eventGen()
+        for(i=0 ; i<10 ; i++) {
+            rest()
+            eventGen()
+        }
     }
 
 } else {
